50ee72737fa7c944dea0ae0956f8ecc60ced7897
teambullback <teambullback@gmail.com>
2014-08-14 18:13:02 +0900

    Some advancements in popup extension, almost done in user experience next thing to do? -> Builder button and Login session(cookie)

--- static/views/popup/static/javascripts/controllers.js
+++ static/views/popup/static/javascripts/controllers.js
@@ -1,10 +1,22 @@
 var $signout = $("<a href='#searchPage' id='signoutIcon' data-toggle='tooltip' data-placement='bottom' title='signout' ng-click='signoutClick()'><i class='fa fa-sign-out'></i></a>")
+var $signinMessage = $("<a href='#signinPage' id='signinMessage' style='text-decoration:underline'>Please Log-in</a>");
+var $signoutMessage = $("<p id='signoutMessage'>Hello {{user}}</p>");
 
 $("#signIcons").append($signout);
 $("#signinIcon").show();
 $("#signoutIcon").hide();
+$("#settingIcon").hide();
+
+$("#signingMessage").append($signinMessage);
+$("#signingMessage").append($signoutMessage);
+$("#signinMessage").show();
+$("#signoutMessage").hide();
 
 var extensionControllers = angular.module('extensionPopup', []);
+
+extensionControllers.controller('signingController', ['$scope', 'functionFactory', function($scope, functionFactory){
+	$scope.user = functionFactory.username;
+}])
 
 extensionControllers.config(['$routeProvider',
 	function($routeProvider){
@@ -58,14 +70,22 @@ extensionControllers.factory('functionFactory', ['$http', '$location', function(
 			"username": id.toString(),
 		    "password": pw.toString()
 		};
+		functionFactory.username = id.toString();
 		$http.post(baseUrl, data).success(function(data){
 			functionFactory["token"] = data;
 			alert("You've logged-in successfully!")
 			$("#loginForm").hide();
 			$("#loginCtrl").append("<p>You've logged-in successfully!</p>")
 			$("#signinIcon").hide();
+			$("#settingIcon").show();
 			$("#signoutIcon").show();
+			$("#signinMessage").hide();
+			$("#signoutMessage").show();
 			$location.path('/searchPage');		
+		}).error(function(data){
+			alert("Login error! Please try again!");
+			$location.path('/signinPage');
+			$(".form-control").val("");
 		})
 	}
 	return functionFactory;
@@ -75,6 +95,9 @@ extensionControllers.controller('singoutIconController', ['$scope', function($sc
 	$scope.signoutClick = function(){
 		alert("You've logged-out successfully!")
 		$("#signoutIcon").hide();
+		$("#settingIcon").hide();
 		$("#signinIcon").show();
+		$("#signinMessage").show();
+		$("#signoutMessage").hide();
 	};
 }]);
