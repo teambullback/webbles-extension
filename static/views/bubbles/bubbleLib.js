dba0e15be57a6773202602fd62625e44b4eb2773
Wonkyung Lyu <me@lyuwonkyung.com>
2014-08-17 00:50:03 +0900

    제작 모드 Bubble UI 구현 완료

--- static/views/bubbles/bubbleLib.js
+++ static/views/bubbles/bubbleLib.js
@@ -1,7 +1,13 @@
 /*===========================================================================
 	Bubble Library
 	included in goDumber Project, Team Bullback
-	Writen by LyuGGang on 14.08.11. ~
+	Writen by LyuGGang(me@lyuwonkyung.com) on 14.08.11. ~
+
+	Required Libraries (Dependencies) :
+	- jQuery (http://jquery.com/)
+	- Bootstrap (http://getbootstrap.com/)
+	- Summernote (http://hackerwins.github.io/summernote/)
+	- Font Awesome (http://fortawesome.github.io/Font-Awesome/)
 ===========================================================================*/
 
 /*===========================================================================
@@ -47,6 +53,7 @@ MM.prototype = {
 	onNewBubbleAddedCallback: null,
 	nowOnFocusedDOMIdx: null,
 	util: null,
+	nowShowingBubble: null,
 
 	/*-----------------------------------------------------------------------
 	// methods
@@ -124,15 +131,16 @@ MM.prototype = {
 	// private
 	evtPlusButtonClicked: function(targetDOM){
 
-		var tempDOM = this.util.getAbsoluteDOMPath(targetDOM);	// for debug
 
-		var getDOM = this.util.getSpecificDOMWithPathObj(tempDOM); // for debug
+		//var tempDOM = this.util.getAbsoluteDOMPath(targetDOM);	// for debug
+
+		//var getDOM = this.util.getSpecificDOMWithPathObj(tempDOM); // for debug
 
 		var self = this;
 
 		// making new speech bubble from templete.
-		//this.util.makeNewSpeechBubble(true);
-
+		this.nowShowingBubble = new speechBubble(this.onNewBubbleAddedCallback);
+		this.nowShowingBubble.makeNewBubble(targetDOM, null);
 
 	}
 
@@ -315,10 +323,216 @@ generalUtil.prototype = {
 /*---------------------------------------------------------------------------
 // constructor
 ---------------------------------------------------------------------------*/
+function speechBubble(onSaveCallback){
+
+	var self = this;
+
+	// freeze constants
+	Object.freeze(this.CONSTS);
+
+	//this.bubble = this.CONSTS.TEMPLATE;
+
+	this.onSaveCallback = onSaveCallback;
 
 
-function speechBubble(){
-
-	
 
 };
+
+/*---------------------------------------------------------------------------
+// prototype
+---------------------------------------------------------------------------*/
+speechBubble.prototype = {
+
+	CONSTS: {
+		TEMPLATE: [
+	          '<div id="__goDumber__popover__" class="popover container-fluid" role="tooltip" style="max-width: 400px;">',
+	          '  <div class="arrow"></div>',
+	          '  <div id="bubble" class="row panel panel-default panel-danger" style="width: 400px;">',
+	          '    <div id="title" class="panel-heading col-xs-12">',
+	          '      <div class="panel-title">',
+	          '        <div id="edit" class="popover-title" >',
+	          '          ',
+	          '        </div>',
+	          '      </div>',
+	          '    </div>',
+	          '    <div id="content" class="panel-body col-xs-12">',
+	          '      <div class="row">',
+	          '        <div class="col-xs-12">',
+	          '          <div id="edit" class="popover-content" >',
+	          '            ',
+	          '          </div>',
+	          '        </div>',
+	          '      </div>',
+	          '      <div class="row">',
+	          '        <div class="col-xs-3" style="padding-right: 0;">',
+	          '          <form class="form-inline" role="form">',
+	          '            <div class="form-group">',
+	          '              <label class="sr-only" for="triggerType">trigger type</label>',
+	          '              <select class="form-control">',
+	          '                <option>next</option>',
+	          '                <option>click</option>',
+	          '              </select>',
+	          '            </div>',
+	          '          </form>',
+	          '        </div>',
+	          '        <div class="col-xs-1">',
+	          '        </div>',
+	          '        <div class="col-xs-4" style="padding-left: 0;">',
+	          '          <button id="__goDumber__bubbleSaveBtn__" class="btn btn-block btn-danger">',
+	          '            <i class="fa fa-check"></i> 저장',
+	          '          </button>',
+	          '        </div>',
+	          '        <div class="col-xs-4" style="padding-left: 0;">',
+	          '          <button id="__goDumber__bubbleCancleBtn__" class="btn btn-block btn-default">',
+	          '            <i class="fa fa-times"></i> 취소',
+	          '          </button>',
+	          '        </div>',
+	          '      </div>',
+	          '    </div>',
+	          '  </div>',
+	          '</div>'
+	    ].join('\n')
+	},
+
+
+    bubble: null,
+    onSaveCallback: null,
+
+	makeNewBubble: function(targetDOM, bubbleData){
+
+		var self = this;
+
+
+		if(bubbleData == null){
+
+			// making mode
+			this.bubble = this.CONSTS.TEMPLATE;
+
+
+
+			// register on target DOM
+	
+
+			// $(targetDOM).on('show.bs.popover', function() {
+
+
+
+			// 	//console.log('on showing event registred!');
+
+			// });
+	
+
+
+			$(targetDOM).popover( {
+		        html: true,
+		        title: function() {
+		          return '수정하려면 클릭하세요';
+		        },
+		        content: function() {
+		          return '수정하려면 클릭하세요';
+		        },
+		        template: this.bubble,
+		        placement: 'right',
+		        trigger: 'manual'
+		    });
+
+
+	        $(targetDOM).popover('show');
+
+
+
+				$("#edit.popover-title").click(function() {
+					self.title_edit();
+					// console.log('title edit clicked!');	 // for debug
+
+				});
+
+				$("#edit.popover-title").css('color', 'rgb(0,0,0)');
+
+				$("#edit.popover-content").click(function() {
+					self.content_edit();
+					// console.log('content edit clicked!');	// for debug
+				});
+
+				$("#edit.popover-content").css('color', 'rgb(0,0,0)');
+
+				$("#__goDumber__bubbleSaveBtn__").click(function() {
+					self.save(targetDOM);
+					// console.log('save btn clicked!');	// for debug
+				});
+
+				$("#__goDumber__bubbleCancleBtn__").click(function() {
+
+
+					self.cancle(targetDOM);
+				});
+
+
+	        //console.log('bubble bubble pop! pop!');	// for debug
+	        //console.log(targetDOM); 	// for debug
+
+
+		}else{
+
+
+			//  user mode
+		}
+
+	},
+
+    title_edit: function() {
+        $('#bubble #title #edit').summernote({
+          airMode: true,
+          airPopover: [
+            ['style', ['style']],
+            ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol']],
+            ['insert', ['link']],
+          ]
+        });
+      },
+
+    content_edit: function() {
+      	$('#bubble #content #edit').summernote({
+       		airMode: true,
+        	airPopover: [
+        		['style', ['style']],
+        		['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+        		['fontsize', ['fontsize']],
+        		['color', ['color']],
+        		['para', ['ul', 'ol']],
+        		['insert', ['link']],
+      		]
+    	});
+    },
+
+    save: function(targetDOM) {
+    	var title = $('#bubble #title #edit').code();
+    	var content = $('#bubble #content #edit').code();
+    	//console.log(title);
+    	//console.log(content);
+
+    	//$('#__goDumber__popover__').destroy();
+    	$(targetDOM).popover('hide');
+    	// $('#bubble #title #edit').destroy();
+    	// $('#bubble #content #edit').destroy();
+    },
+
+    cancle: function(targetDOM){
+
+
+    	$(targetDOM).popover('hide');
+    	$('#__goDumber__popover__').destroy();
+    }
+
+
+
+
+
+
+        
+
+
+}
