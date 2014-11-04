1bfb94c9b345b0b55c1bbc498e452cbc9d912cbc
Kim Changyeun <magnate0@naver.com>
2014-11-04 15:18:02 +0900

    1.버전업 2.엔딩모달 끌떄 페이지이동안하고 꺼지게 수정

--- static/javascripts/endingApp.js
+++ static/javascripts/endingApp.js
@@ -244,22 +244,12 @@
                     }
 
                     $scope.closeWebbles = function() {
-                        var moving_url;
-                        chrome.storage.local.get("tutorials", function(data) {
-                            var parse_tutorials = JSON.parse(data.tutorials);
-                            var parse_bubbles = JSON.parse(parse_tutorials.bubbles);
-
-                            for (var list in parse_bubbles) {
-                                if (!parse_bubbles[list].prev) {
-                                    moving_url = parse_bubbles[list].page_url;
-                                }
-                            }
-
-                            // console.log("THIS IS MOVING_URL ===>", moving_url);
-                            contentScriptsPort.postMessage({
-                                type: "exit_user_mode",
-                                data: moving_url
-                            });
+                        contentScriptsPort.postMessage({
+                            type: "exit_user_mode",
+                            data: moving_url
+                        }, function(){
+                            $('#bubblemap_user').remove();
+                            alert("위블즈가 종료되었습니다! 사용에 감사드립니다!");
                         });
                     }
 
